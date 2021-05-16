5dd433242f3f08f73a70dec29899c1655a0cfeef
Lennert Verbesselt <lennert@verbesselt.net>
2021-05-16 19:23:16 +0200

    Changed typo in CMS/challenges

--- resources/views/CMS/challenges.blade.php
+++ resources/views/CMS/challenges.blade.php
@@ -144,7 +144,7 @@
                                     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                             <div class="ml-3">
                                                 <p class="text-gray-900 whitespace-no-wrap">
-                                                    {{$challenge->id}}
+                                                    {{$challenge->name}}
                                                 </p>
                                             </div>
                                         
