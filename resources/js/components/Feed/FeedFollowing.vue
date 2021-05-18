e26723050e0920cfa43bd77a190d9a0c030cc293
Lucas Bartucci <you@example.com>
2021-05-18 12:24:24 +0100

    Following button function added

--- resources/js/components/Feed/FeedFollowing.vue
+++ resources/js/components/Feed/FeedFollowing.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="button">
-    <button v-on:click="message = 'Following'">Follow</button>
+    <button :class="[isActive ? 'fill' : 'clicked']" @click="isActive = !isActive" >{{isActive ? 'Follow' : 'Following'}}</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data:function() {
+        return {
+            isActive: true
+        }
+    }
 }
 </script>
 
@@ -14,10 +18,16 @@ export default {
 .button {
     padding-top: 18px;
 }
-    button {
+    .fill {
         background-color: #EB5876;
         color: white;
         border-radius: 4px;
         border: none;
+    }
+    .clicked {
+        background-color: transparent;
+        border: 2px solid #EB5876;
+        border-radius: 4px;
+        color: white;
     }
 </style>
