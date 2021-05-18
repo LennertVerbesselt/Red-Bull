24af9967a29b56b3ae73d8c9e6b30a485d626a22
Lennert Verbesselt <lennert@verbesselt.net>
2021-05-18 10:38:53 +0200

    Adding authentication to vue login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RegisteredUserController;
+use App\Http\Controllers\API\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
--- routes/auth.php
+++ routes/auth.php
@@ -14,15 +14,15 @@ use Illuminate\Support\Facades\Route;
                 ->middleware('guest')
                 ->name('register');*/
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
+/*Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest');
 
 /*Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
                 ->name('login');*/
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest');
+/*Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+                ->middleware('guest');*/
 
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->middleware('guest')
